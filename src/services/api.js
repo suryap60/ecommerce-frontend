bc045ea944557eb3498715ee02009a16b53cda32
suryap60 <suryap6061@gmail.com>
2026-03-11 21:56:59 +0530

    fix: server issue

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-// Determine if we are in production or development
-const isProduction = window.location.hostname !== "localhost";
-
 // Use Render backend in production, localhost in dev
-const API_BASE_URL = isProduction
-  ? "https://ecommerce-backend-u0t5.onrender.com/api" // Production backend
-  : "http://localhost:5000/api";                       // Dev backend
+const API_BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000/api"  // dev
+    : "https://ecommerce-backend-u0t5.onrender.com/api"; // production
 
 export const API = axios.create({
   baseURL: API_BASE_URL,
